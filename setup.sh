c94fe9ec95a97cacc8feadb4dc7117b6a538ec2c
Boby Santoso F <bfreedantius@gmail.com>
2025-01-01 16:50:39 +0700

    feat: setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define packages
+packages="stow zsh neovim tmux git ripgrep"
+stow_folders="zsh nvim tmux"
+
+# Install dependencies
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # MacOS
+    brew update
+    brew install $packages
+else
+    # Linux
+    sudo apt update
+    sudo apt install -y $packages
+fi
+
+# Stow configurations
+stow $stow_folders
+
+# Set Zsh as default shell
+chsh -s $(which zsh)
+
+echo "Dotfiles setup complete!"
+
