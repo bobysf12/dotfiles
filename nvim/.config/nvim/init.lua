6fe87c70ca0e059dee90af62d1709278947b0499
Boby Santoso F <bfreedantius@gmail.com>
2025-07-18 11:00:55 +0000

    feat: Update blink

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -407,17 +407,8 @@ require("lazy").setup({
 			{ "williamboman/mason.nvim", config = true },
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
-
-			-- Blink CMP with optional setup (customize via `opts` if needed)
-			{ "saghen/blink.cmp", opts = {} },
-
-			-- Required for default LSP capabilities
-			"hrsh7th/cmp-nvim-lsp",
-
-			-- Optional but useful UI improvements
 			{ "j-hui/fidget.nvim", opts = {} },
 		},
-
 		config = function()
 			-- Setup event for LSP attachment to buffers
 			vim.api.nvim_create_autocmd("LspAttach", {
@@ -488,12 +479,21 @@ require("lazy").setup({
 				end,
 			})
 
-			-- Create base capabilities from cmp-nvim-lsp
-			local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-			-- Merge with blink.cmp capabilities
-			local capabilities = require("blink.cmp").get_lsp_capabilities(cmp_capabilities)
+			-- LSP servers and clients are able to communicate to each other what features they support.
+			--  By default, Neovim doesn't support everything that is in the LSP specification.
+			--  When you add blink.cmp, Neovim now has *more* capabilities.
+			--  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			-- Define servers and their settings
+			-- Enable the following language servers
+			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+			--
+			--  Add any additional override configuration in the following tables. Available keys are:
+			--  - cmd (table): Override the default command used to start the server
+			--  - filetypes (table): Override the default list of associated filetypes for the server
+			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+			--  - settings (table): Override the default settings passed when initializing the server.
+			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				ts_ls = {},
 				intelephense = {},
@@ -572,123 +572,6 @@ require("lazy").setup({
 				typescriptreact = { "prettierd", stop_after_first = true },
 			},
 		},
-	},
-
-	{ -- Autocompletion
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			-- Snippet Engine & its associated nvim-cmp source
-			{
-				"L3MON4D3/LuaSnip",
-				build = (function()
-					-- Build Step is needed for regex support in snippets.
-					-- This step is not supported in many windows environments.
-					-- Remove the below condition to re-enable on windows.
-					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-						return
-					end
-					return "make install_jsregexp"
-				end)(),
-				dependencies = {
-					-- `friendly-snippets` contains a variety of premade snippets.
-					--    See the README about individual language/framework/plugin snippets:
-					--    https://github.com/rafamadriz/friendly-snippets
-					{
-						"rafamadriz/friendly-snippets",
-						config = function()
-							require("luasnip.loaders.from_vscode").lazy_load()
-						end,
-					},
-				},
-			},
-			"saadparwaiz1/cmp_luasnip",
-
-			-- Adds other completion capabilities.
-			--  nvim-cmp does not ship with all sources by default. They are split
-			--  into multiple repos for maintenance purposes.
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-		},
-		config = function()
-			-- See `:help cmp`
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			luasnip.config.setup({})
-
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				completion = { completeopt = "menu,menuone,noinsert" },
-
-				-- For an understanding of why these mappings were
-				-- chosen, you will need to read `:help ins-completion`
-				--
-				-- No, but seriously. Please read `:help ins-completion`, it is really good!
-				mapping = cmp.mapping.preset.insert({
-					-- Select the [n]ext item
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					-- Select the [p]revious item
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-
-					-- Scroll the documentation window [b]ack / [f]orward
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-					-- Accept ([y]es) the completion.
-					--  This will auto-import if your LSP supports it.
-					--  This will expand snippets if the LSP sent a snippet.
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					-- ['<CR>'] = cmp.mapping.confirm { select = true },
-
-					-- If you prefer more traditional completion keymaps,
-					-- you can uncomment the following lines
-					--['<CR>'] = cmp.mapping.confirm { select = true },
-					--['<Tab>'] = cmp.mapping.select_next_item(),
-					--['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-					-- Manually trigger a completion from nvim-cmp.
-					--  Generally you don't need this, because nvim-cmp will display
-					--  completions whenever it has completion options available.
-					["<C-Space>"] = cmp.mapping.complete({}),
-
-					-- Think of <c-l> as moving to the right of your snippet expansion.
-					--  So if you have a snippet that's like:
-					--  function $name($args)
-					--    $body
-					--  end
-					--
-					-- <c-l> will move you to the right of each of the expansion locations.
-					-- <c-h> is similar, except moving you backwards.
-					["<C-l>"] = cmp.mapping(function()
-						if luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						end
-					end, { "i", "s" }),
-					["<C-h>"] = cmp.mapping(function()
-						if luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						end
-					end, { "i", "s" }),
-
-					-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-				}),
-				sources = {
-					{
-						name = "lazydev",
-						-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-						group_index = 0,
-					},
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "path" },
-				},
-			})
-		end,
 	},
 
 	{ -- You can easily change to a different colorscheme.
--- setup.sh
+++ setup.sh
@@ -1,18 +1,73 @@
 #!/bin/bash
 
 # Define packages
-packages="stow zsh neovim tmux git fzf eza duf fastfetch lazygit watson thefuck zoxide ripgrep slides"
+apt_packages="stow zsh neovim tmux git fzf ripgrep"
+non_apt_packages="eza duf fastfetch lazygit watson thefuck zoxide slides"
 stow_folders="zsh nvim tmux ghostty yazi tmux-sessionizer bin"
 
 # Install dependencies
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # MacOS
     brew update
-    brew install $packages
+    brew install $apt_packages $non_apt_packages
 else
     # Linux
     sudo apt update
-    sudo apt install -y $packages
+    sudo apt install -y $apt_packages
+
+    # Install non-apt packages
+    # eza
+    if ! command -v eza &> /dev/null; then
+        curl -fsSL https://github.com/ogham/exa/releases/latest/download/eza-linux-x86_64.zip -o eza.zip
+        unzip eza.zip -d eza_tmp
+        sudo mv eza_tmp/eza /usr/local/bin/
+        rm -rf eza.zip eza_tmp
+    fi
+
+    # duf
+    if ! command -v duf &> /dev/null; then
+        curl -fsSL https://github.com/muesli/duf/releases/latest/download/duf_0.8.1_linux_amd64.deb -o duf.deb
+        sudo dpkg -i duf.deb
+        rm duf.deb
+    fi
+
+    # fastfetch
+    if ! command -v fastfetch &> /dev/null; then
+        curl -fsSL https://github.com/LinusDierheimer/fastfetch/releases/latest/download/fastfetch-linux-x86_64 -o fastfetch
+        sudo install fastfetch /usr/local/bin/
+        rm fastfetch
+    fi
+
+    # lazygit
+    if ! command -v lazygit &> /dev/null; then
+        curl -fsSL https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_amd64.deb -o lazygit.deb
+        sudo dpkg -i lazygit.deb
+        rm lazygit.deb
+    fi
+
+    # watson
+    if ! command -v watson &> /dev/null; then
+        pip3 install --user td-watson
+    fi
+
+    # thefuck
+    if ! command -v thefuck &> /dev/null; then
+        sudo apt install -y python3-pip
+        pip3 install --user thefuck
+    fi
+
+    # zoxide
+    if ! command -v zoxide &> /dev/null; then
+        curl -fsSL https://github.com/ajeetdsouza/zoxide/releases/latest/download/zoxide-x86_64-linux.tar.gz | tar -xzf - -C /tmp
+        sudo install /tmp/zoxide /usr/local/bin/
+    fi
+
+    # slides
+    if ! command -v slides &> /dev/null; then
+        curl -fsSL https://github.com/maaslalani/slides/releases/latest/download/slides-linux-amd64 -o slides
+        sudo install slides /usr/local/bin/
+        rm slides
+    fi
 fi
 
 # Stow configurations
