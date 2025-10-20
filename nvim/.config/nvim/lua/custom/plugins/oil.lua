6570a55d165076151700825eea2446a293fa5348
Boby Santoso F <bfreedantius@gmail.com>
2025-10-20 10:31:26 +0700

    feat: Update git flow

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -2,12 +2,15 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
 	-- Optional dependencies
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			win_options = {
+				signcolumn = "yes:2",
+			},
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
